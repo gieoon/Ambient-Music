d271fd413fad4e402cfb94304e25fc822583bf6c
gieoon <jun.a.kagaya@gmail.com>
2020-08-10 20:08:12 +1200

    Updates

---
+++ precache-manifest.51a062b670cea7c72fdfbdd958f07981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c6a254447450e7fbf131981d2f354b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea598ec854125721ca56",
+    "url": "/static/css/main.f8c61ffb.chunk.css"
+  },
+  {
+    "revision": "92236bb4e447f9d8bb13",
+    "url": "/static/js/2.bcf81c76.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bcf81c76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea598ec854125721ca56",
+    "url": "/static/js/main.d29544f4.chunk.js"
+  },
+  {
+    "revision": "526876ffeec74214774c",
+    "url": "/static/js/runtime-main.131151fc.js"
+  }
+]);
